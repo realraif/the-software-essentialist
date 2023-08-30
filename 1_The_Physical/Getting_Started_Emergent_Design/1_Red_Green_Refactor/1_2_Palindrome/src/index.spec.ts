f1b1bdcdd194cfa853483b0d7893cdbb44440115
Yael <raifer.dev@gmail.com>
2023-08-30 13:51:09 +0300

    removed comments

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -2,16 +2,6 @@ import { palindromeChecker } from './index';
 
 describe('palindrome checker', () => {
 
-  // "mom" returns true
-  // "Mom" returns true
-  // "MoM" returns true
-  // "Momx" returns false
-  // "xMomx" returns true
-  // "Was It A Rat I Saw" returns true
-  // "Never Odd or Even" returns true
-  // "Never Odd or Even1" returns false 
-  // "1Never Odd or Even1" returns true
-
   describe('returns true for palindrom str', () => {
     it.each(['mom', 'MoM'])('knows that %s is a palindrome', (str: string) => {
       expect(palindromeChecker(str)).toBe(true);
