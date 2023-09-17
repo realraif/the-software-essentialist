7e51464a3464b8c8aaabbacb2828ad5b975f8773
Yael <raifer.dev@gmail.com>
2023-09-17 20:03:40 +0300

    more examples

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -35,6 +35,27 @@ describe('password validator', () => {
     expect(validationResponse.errors[0].message).toContain('at least one upper case letter');
   });
 
+  it('should know that "Mona" is invalid', () => {
+    const data = "Mona";
+    const validationResponse = passwordValidator.validate(data);
+
+    expect(validationResponse.result).toBe(false);
+    expect(validationResponse.errors).toBeDefined();
+    expect(validationResponse.errors.length).toEqual(2);
+    expect(validationResponse.errors[0].message).toContain('between 5 and 15');
+    expect(validationResponse.errors[1].message).toContain('at least one digit');
+  });
+
+  it('should know that "Mom1234566778855" is invalid', () => {
+    const data = "Mom1234566778855";
+    const validationResponse = passwordValidator.validate(data);
+
+    expect(validationResponse.result).toBe(false);
+    expect(validationResponse.errors).toBeDefined();
+    expect(validationResponse.errors.length).toEqual(1);
+    expect(validationResponse.errors[0].message).toContain('between 5 and 15');
+  });
+
   it('should know that "mom123A" is valid', () => {
     const data = "mom123A";
     const validationResponse = passwordValidator.validate(data);
