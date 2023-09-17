ff33881b69ecb7a708d903a5ade73a64de2f583c
Yael <raifer.dev@gmail.com>
2023-09-17 19:39:48 +0300

    validated 'mom' by wishful thinking

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -33,8 +33,15 @@ example:
 
 describe('password validator', () => {
 
-  test('hello', () => {
-    expect("between 5 and 15").toContain('5 and 15')
+  it('should know that "mom" is invalid', () => {
+    const data = "mom";
+    const validationResponse = passwordValidator.validate(data);
+
+    expect(validationResponse.result).toBe(false);
+    expect(validationResponse.errors).toBeDefined();
+    expect(validationResponse.errors.length).toEqual(3);
+    expect(validationResponse.errors[0].message).toContain('between 5 and 15');
+    expect(validationResponse.errors[1].message).toContain('at least one digit');
+    expect(validationResponse.errors[2].message).toContain('at least one upper case letter');
   })
 })
-
--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,6 +1,13 @@
 
 export class passwordValidator {
-  static validate(password: string): boolean {
-    return true
+  static validate(password: string): any {
+    return {
+      result: false,
+      errors: [
+        { type: 'length', message: 'between 5 and 15' },
+        { type: 'digit', message: 'at least one digit' },
+        { type: 'upper', message: 'at least one upper case letter' }
+      ]
+    }
   }
 }
