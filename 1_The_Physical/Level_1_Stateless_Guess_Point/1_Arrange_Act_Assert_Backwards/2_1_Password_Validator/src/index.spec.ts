9796637635e01be4175e3500873a613b504f8455
Yael <raifer.dev@gmail.com>
2023-09-17 19:56:57 +0300

    removed comments

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,37 +1,6 @@
 
 import { passwordValidator } from './index'
 
-/*
-
-  validate password
-
-  criteria:
-Between 5 and 15 characters long
-Contains at least one digit
-Contains at least one upper case letter
-
-examples:
-mom -> false
-mom123 -> false
-mom123a -> false
-mom123A -> true
-
-Return an object
-containing a boolean result
-an errors key that contains an error message or type for all errors in occurrence.
-
-example:
-{
-  result: false,
-  errors: [
-    { type: 'length', message: 'between 5 and 15' },
-    { type: 'digit', message: 'at least one digit' },
-    { type: 'upper', message: 'at least one upper case letter' }
-  ]
-}
-
-*/
-
 describe('password validator', () => {
 
   it('should know that "mom" is invalid', () => {
