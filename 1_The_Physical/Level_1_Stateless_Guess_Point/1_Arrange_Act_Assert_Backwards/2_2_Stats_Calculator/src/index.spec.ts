95c7286ef11a1aed3700acf3f38af2e6ec00ed40
Yael <raifer.dev@gmail.com>
2023-09-19 20:31:34 +0300

    used it.each() parameterization on duplications

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,21 +1,5 @@
 import { StatsCalculator } from './index';
 
-/*
-  process a sequence of integer numbers to determine the following statistics:
-
-  Finds the minimum value in the sequence
-  [0, 1, 2]
-  [-1, 0, 100, 12]
-
-  Finds the maximum value in the sequence
-
-  Finds the number of elements in the sequence
-
-  Calculates the average value of the sequence
-
-  returns 0 for all values if the sequence is empty
-
-*/
 
 describe('stats calculator', () => {
 
@@ -29,60 +13,51 @@ describe('stats calculator', () => {
     expect(result.average).toBe(0);
   });
 
-  it('knows that the min value of [0, 1, 2] is 0', () => {
-    let series: number[] = [0, 1, 2];
-    let result = StatsCalculator.processSeries(series);
 
-    expect(result.min).toBe(0);
+  describe('finds the minimum value in the sequence', () => {
+
+    it.each([
+      [[0, 1, 2], 0],
+      [[-1, 0, 100, 12], -1]
+    ])('knows that the min value of %p is %i', (series: number[], expected: number) => {
+      let result = StatsCalculator.processSeries(series);
+
+      expect(result.min).toBe(expected);
+    });
+
   });
 
-  it('knows that the min value of [-1, 0, 100, 12] is -1', () => {
-    let series: number[] = [-1, 0, 100, 12];
-    let result = StatsCalculator.processSeries(series);
+  describe('finds the maximum value in the sequence', () => {
+    it.each([
+      [[0, 1, 2], 2],
+      [[-1, 0, 100, 12], 100]
+    ])('knows that the max value of %p is %i', (series: number[], expected: number) => {
+      let result = StatsCalculator.processSeries(series);
 
-    expect(result.min).toBe(-1);
+      expect(result.max).toBe(expected);
+    });
   });
 
-  it('knows that the max value of [0, 1, 2] is 2', () => {
-    let series: number[] = [0, 1, 2];
-    let result = StatsCalculator.processSeries(series);
+  describe('finds the number of elements in the sequence', () => {
+    it.each([
+      [[0, 1, 2], 3],
+      [[-1, 0, 100, 12], 4]
+    ])('knows that the count of %p is %i', (series: number[], expected: number) => {
+      let result = StatsCalculator.processSeries(series);
 
-    expect(result.max).toBe(2);
+      expect(result.count).toBe(expected);
+    });
   });
 
-  it('knows that the max value of [-1, 0, 100, 12] is 100', () => {
-    let series: number[] = [-1, 0, 100, 12];
-    let result = StatsCalculator.processSeries(series);
+  describe('calculates the average value of the sequence', () => {
+    it.each([
+      [[0, 1, 2], 1],
+      [[-1, 0, 100, 12], 27.75]
+    ])('knows that the average of %p is %i', (series: number[], expected: number) => {
+      let result = StatsCalculator.processSeries(series);
 
-    expect(result.max).toBe(100);
-  });
-
-  it ('knows that the count of [0, 1, 2] is 3', () => {
-    let series: number[] = [0, 1, 2];
-    let result = StatsCalculator.processSeries(series);
-
-    expect(result.count).toBe(3);
-  });
-
-  it('knows that the count of [-1, 0, 100, 12] is 4', () => {
-    let series: number[] = [-1, 0, 100, 12];
-    let result = StatsCalculator.processSeries(series);
-
-    expect(result.count).toBe(4);
-  });
-
-  it('knows that the average of [0, 1, 2] is 1', () => {
-    let series: number[] = [0, 1, 2];
-    let result = StatsCalculator.processSeries(series);
-
-    expect(result.average).toBe(1);
-  });
-
-  it('knows that the average of [-1, 0, 100, 12] is 27.75', () => {
-    let series: number[] = [-1, 0, 100, 12];
-    let result = StatsCalculator.processSeries(series);
-
-    expect(result.average).toBe(27.75);
+      expect(result.average).toBe(expected);
+    });
   });
 
 })
