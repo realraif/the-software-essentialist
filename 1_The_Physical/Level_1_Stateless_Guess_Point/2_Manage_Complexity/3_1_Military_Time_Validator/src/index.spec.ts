5b2132e8a1b6595c6236c5235e560e2f348b27d0
Yael <raifer.dev@gmail.com>
2023-09-20 22:12:44 +0300

    used it.each() parameterization and added examples

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -100,16 +100,16 @@ describe('military time validator', () => {
     });
   });
 
-  it('knows that for "01:12 - 14:32" the minutes are valid', () => {
-    const isTimeValid = MilitatyTimeValidator.validate("01:12 - 14:32");
+  describe('validates that the minutes are valid', () => {
+    it.each([
+      ["01:12 - 14:32", true],
+      ["01:60 - 14:32", false],
+      ["01:12 - 14:72", false],
+    ])('knows that "%s" returns %s', (time, expected) => {
+      const isTimeValid = MilitatyTimeValidator.validate(time);
 
-    expect(isTimeValid).toBe(true);
-  });
-
-  it('knows that for "01:60 - 14:32" the minutes are not valid', () => {
-    const isTimeValid = MilitatyTimeValidator.validate("01:60 - 14:32");
-
-    expect(isTimeValid).toBe(false);
+      expect(isTimeValid).toBe(expected);
+    });
   });
 
 })
